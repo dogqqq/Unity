668feb7eafb065d153c91977a2f6887f9fba0f7f
dogQ <dog98726345@gmail.com>
2020-03-27 17:32:27 +0800

    Add camera-controller, walls

---
+++ Pool/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rg;
+    public float speed;
+    private void Start()
+    {
+        rg = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rg.AddForce(movement * speed);
+    }
+}
