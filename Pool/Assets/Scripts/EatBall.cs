8c31708c038b4ee685dedb5f204bd9e2d849591e
dogQ <dog98726345@gmail.com>
2020-04-17 18:20:45 +0800

    Add Holls, collision

---
+++ Pool/Assets/Scripts/EatBall.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EatBall : MonoBehaviour
+{
+	void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Ball")){
+            other.gameObject.SetActive(false);
+        }
+    }
+
+}
