f81450d345aacda6f16ac37072271f687a78c489
dogQ <dog98726345@gmail.com>
2020-05-08 18:07:14 +0800

    Finish Balling

---
+++ Balling/Assets/Scripts/BottleControl.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BottleControl : MonoBehaviour
+{
+	public Text countText;
+    public Text strikeText;
+
+    private static int count;
+    private float center;
+    private bool check;
+    private Rigidbody rg;
+    // Start is called before the first frame update
+    void Start()
+    {
+        count = 0;
+        strikeText.text = "";
+        check = false;
+        SetText();
+        rg = GetComponent<Rigidbody>();
+        center = rg.position.y + rg.centerOfMass.y;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate(){
+        if(rg.position.y + rg.centerOfMass.y < center-0.1 && !check){
+        	count += 1;
+        	check = true;
+        }
+        SetText();
+    }
+
+    void SetText(){
+    	countText.text = "Count: " + count.ToString();
+    	if(count >= 10){
+    		strikeText.text = "Strike!!!!!";
+    	}
+    }
+}
---
+++ Balling/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+	private Rigidbody rg;
+    // Start is called before the first frame update
+    private void Start()
+    {
+    	rg = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        if(moveVertical != 0.0f || moveHorizontal != 0.0f){
+        	Application.LoadLevel(0);
+        }
+
+    }
+
+}
